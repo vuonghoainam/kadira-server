8e112ec5baf43a63c80e1dc243f5d1fb55d4f173
nam's laptop <vuonghoainam@gmail.com>
2017-06-27 10:15:07 +0700

    Change some ENV vars

--- init-shell.sh
+++ init-shell.sh
@@ -1,8 +1,10 @@
+# init-shell.sh
+
 # DB settings
-export APP_MONGO_URL="mongodb://app:password@candidate.53.mongolayer.com:10478,candidate.54.mongolayer.com:10216/tkadira-app?replicaSet=set-56175e62147ca745d8000761"
-export APP_MONGO_OPLOG_URL="mongodb://oplog:password@candidate.53.mongolayer.com:10478,candidate.54.mongolayer.com:10216/local?authSource=tkadira-app&replicaSet=set-56175e62147ca745d8000761"
-export DATA_MONGO_URL="mongodb://app:password@candidate.53.mongolayer.com:10478,candidate.54.mongolayer.com:10216/tkadira-data?replicaSet=set-56175e62147ca745d8000761"
-export MAIL_URL="smtp://postmaster%40kadira.io:9jx4fqhdfbg5@smtp.mailgun.org:587"
+export APP_MONGO_URL="mongodb://dev:chord@localhost:27017/test?replicaSet=rs0"
+export APP_MONGO_OPLOG_URL="mongodb://dev:chord@localhost:27017/local?authSourc$"
+export DATA_MONGO_URL="mongodb://dev:chord@localhost:27017/test?replicaSet=rs0"
+#export MAIL_URL="smtp://postmaster%40kadira.io:9jx4fqhdfbg5@smtp.mailgun.org:5$"
 
 # Engine settings
 export ENGINE_PORT=11011
